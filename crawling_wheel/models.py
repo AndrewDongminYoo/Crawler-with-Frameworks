0b261b796e5fa07cf1bfc975cc03bec97174f514
ydm2790 <ydm2790@gmail.com>
2022-01-27 17:57:49 +0900

    model set

--- crawling_wheel/models.py
+++ crawling_wheel/models.py
@@ -1,15 +1,29 @@
 import uuid
 import bson
+import json
 import mongoengine as me
 
 
 class CatFood(me.Document):
-    site = me.URLField()
-    url = me.URLField()
-    brand = me.StringField()
-    title = me.StringField()
-    image = me.URLField()
-    analysis = me.StringField()
-    ingredients = me.StringField()
-    calorie = me.StringField()
-    additives = me.StringField()
+    def __str__(self):
+        return json.dumps({
+            "site": self.site,
+            "url": self.url,
+            "brand": self.brand,
+            "title": self.title,
+            "image": self.image,
+            "analysis": self.analysis,
+            "ingredients": self.ingredients,
+            "calorie": self.calorie,
+            "additives": self.additives,
+        }, ensure_ascii=False)
+
+    site = me.URLField(default="")
+    url = me.URLField(default="")
+    brand = me.StringField(default="")
+    title = me.StringField(default="")
+    image = me.URLField(default="")
+    analysis = me.StringField(default="")
+    ingredients = me.StringField(default="")
+    calorie = me.StringField(default="")
+    additives = me.StringField(default="")
