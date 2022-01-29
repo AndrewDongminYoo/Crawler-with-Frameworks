c02b22c469a500786a363e2faf6368ca3e7145ab
ydm2790 <ydm2790@gmail.com>
2022-01-29 13:55:55 +0900

    application :: get_google_image_url

--- crawling_wheel/google_get_img.py
+++ crawling_wheel/google_get_img.py
@@ -3,6 +3,7 @@ from urllib.parse import urlparse
 from pymongo import MongoClient
 from selenium.webdriver import Chrome
 from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
@@ -13,7 +14,7 @@ driver.implicitly_wait(5)
 wait = WebDriverWait(driver, timeout=5)
 
 
-def finda(XPATH):
+def finda(XPATH) -> WebElement:
     return wait.until(EC.element_to_be_clickable((By.XPATH, XPATH)))
 
 
@@ -23,9 +24,11 @@ def search_google_img_get_first(product):
         url = product['url']
         driver.get(f"https://www.google.com/search?q={url}&tbm=isch")
         finda('//*[@id="islrg"]/div[1]/div[1]/a[1]').click()
-        finda('//*[@id="Sva75c"]/div/div/div[3]/div[2]/c-wiz/div/div[1]/div[1]/div[1]/a[1]').click()
-        finda('//*[@id="Sva75c"]/div/div/div[3]/div[2]/c-wiz/div/div[1]/div[1]/div[1]/div[2]/div/a').click()
-        return finda('//*[@id="yDmH0d"]/div[6]/div/div[2]/span/div/div/div[4]/a').text
+        return finda('//*[@id="Sva75c"]/div/div/div[3]/div[2]/c-wiz/div/div[1]/div[1]/div[2]/div/a/img').get_attribute(
+            "src")
+        # finda('//*[@id="Sva75c"]/div/div/div[3]/div[2]/c-wiz/div/div[1]/div[1]/div[1]/a[1]').click()
+        # finda('//*[@id="Sva75c"]/div/div/div[3]/div[2]/c-wiz/div/div[1]/div[1]/div[1]/div[2]/div/a').click()
+        # return finda('//*[@id="yDmH0d"]/div[6]/div/div[2]/span/div/div/div[4]/a').text
     except TimeoutException:
         logger.warning(product)
 
