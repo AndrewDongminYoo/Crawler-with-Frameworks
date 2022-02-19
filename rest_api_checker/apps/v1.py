693de29180438c543db31779be9a8958dc37bc1e
dongminyoo <andrew@i-ve.kr>
2022-02-19 18:47:34 +0900

    requirements

--- rest_api_checker/apps/v1.py
+++ rest_api_checker/apps/v1.py
@@ -10,7 +10,7 @@ db = client.get_database("cat")
 col = db.get_collection("CatFood_test006")
 
 
-@app.route('/cat-foods', methods=['GET', 'PATCH'])
+@app.route('/cat-foods', methods=['GET'])
 def call_cat_foods():
     args = request.args
     if request.method == "GET":
@@ -18,10 +18,3 @@ def call_cat_foods():
         per_page = 60
         cat_foods = col.find({}, {"_id": False}).skip((page - 1) * per_page).limit(per_page)
         return jsonify(list(cat_foods))
-    elif request.method == "PATCH":
-        title = args.get("title")
-        target = col.find_one({"title": title})
-        cat_food = CatFood().from_dict(target)
-        cat_food.image = search_google_image_with_title(title)
-        col.update_one({"title": cat_food.title}, {"$set": cat_food.to_mongo()})
-        return cat_food.to_json()
